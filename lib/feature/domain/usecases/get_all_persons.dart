33805438cc7ad8e6ed418309cfe3a4a5b4acbf86
Решетов Денис <71463564+reshetov-denis@users.noreply.github.com>
2025-11-16 00:07:59 +0300

    Добавить абстрактный класс UseCase

--- lib/feature/domain/usecases/get_all_persons.dart
+++ lib/feature/domain/usecases/get_all_persons.dart
@@ -1,15 +1,27 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 
 import 'package:rick_and_morty_api/core/error/failure.dart';
+import 'package:rick_and_morty_api/core/usecases/usecase.dart';
 import 'package:rick_and_morty_api/feature/domain/repositories/person_repository.dart';
 import 'package:rick_and_morty_api/feature/domain/entities/person_entity.dart';
 
-class GetAllPersons {
+class GetAllPersons extends UseCase<List<PersonEntity>, PagePersonParams> {
   final PersonRepository personRepository;
 
   GetAllPersons(this.personRepository);
 
-  Future<Either<Failure, List<PersonEntity>>> call(int page) async {
-    return await personRepository.getAllPersons(page);
+  Future<Either<Failure, List<PersonEntity>>> call(PagePersonParams params) async {
+    return await personRepository.getAllPersons(params.page);
   }
+}
+
+class PagePersonParams extends Equatable {
+  final int page;
+
+  const PagePersonParams({required this.page});
+
+  @override
+  List<Object?> get props => [];
+
 }
--- lib/feature/domain/usecases/search_person.dart
+++ lib/feature/domain/usecases/search_person.dart
@@ -1,15 +1,26 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 
 import 'package:rick_and_morty_api/core/error/failure.dart';
+import 'package:rick_and_morty_api/core/usecases/usecase.dart';
 import 'package:rick_and_morty_api/feature/domain/repositories/person_repository.dart';
 import 'package:rick_and_morty_api/feature/domain/entities/person_entity.dart';
 
-class SearchPerson {
+class SearchPerson extends UseCase<List<PersonEntity>, SearchPersonParams> {
   final PersonRepository personRepository;
 
   SearchPerson(this.personRepository);
 
-  Future<Either<Failure, List<PersonEntity>>> call(String query) async {
-    return await personRepository.searchPerson(query);
+  Future<Either<Failure, List<PersonEntity>>> call(SearchPersonParams params) async {
+    return await personRepository.searchPerson(params.query);
   }
+}
+
+class SearchPersonParams extends Equatable {
+  final String query;
+
+  const SearchPersonParams({required this.query});
+
+  @override
+  List<Object?> get props => [];
 }
